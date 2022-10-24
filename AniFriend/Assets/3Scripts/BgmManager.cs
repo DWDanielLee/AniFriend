30b8c80692273e9a6451ffa44802e5f67691aede
DanielLeear16 <kei01171@naver.com>
2022-10-24 16:14:16 +0900

    Update BGM code, Add BGM_Main in 5Play Scene

--- AniFriend/Assets/3Scripts/BgmManager.cs
+++ AniFriend/Assets/3Scripts/BgmManager.cs
@@ -1,8 +1,16 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public sealed class BgmManager : MonoBehaviour {
     void Start() {
         var bgm = FindObjectsOfType<BgmManager>();
         if (bgm.Length > 1) Destroy(gameObject);
+        DontDestroyOnLoad(this.gameObject);
+    }
+    void Update() {
+        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByBuildIndex(4))
+        {
+            Destroy(gameObject);
+        }
     }
 }
